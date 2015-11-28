7c62ab38c574500cba61a1927b0eef7001edcfe4
yakim-rachev <jakimcho@gmail.com>
2015-11-28 23:30:06 +0000

    Configured it with MongoLabs cloud

--- transmit_msg.py
+++ transmit_msg.py
@@ -5,9 +5,17 @@ from pymongo import MongoClient
 
 def send(message):
 #  for temp in message:
-#    print("Sending Messages" + json.dumps(message))
-  client = MongoClient('mongodb://192.168.13.104:27017/')
-  db = client['rachev']
+  print("Sending Messages" + json.dumps(message))
+  print("pymongo version " + pymongo.__version__)
+# Old mongo connection: client = MongoClient('mongodb:// 192.168.13.104:27017/')
+
+  client = MongoClient('mongodb://jakim:12345@ds059654.mongolab.com:59654/mongojr')
+  
+# old db: db = client['rachev']
+  dd = client.get_default_database()
+  print(dd.collection_names())
+
+  db = client['mongojr']
   temperature = db.temperature
   record_id = temperature.insert(message)
   client.close()
