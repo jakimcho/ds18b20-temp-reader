d9c93ae235e5af10b83ebbb6e5f384843825f132
yakim-rachev <jakimcho@gmail.com>
2015-11-29 19:24:14 +0000

    Pritns multible sensors data to mongo

--- temp_sensor.py
+++ temp_sensor.py
@@ -13,30 +13,46 @@ os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
 
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+devices_folders = glob.glob(base_dir + '28*')
 
-def read_temp_raw():
-  f = open(device_file, 'r')
+def list_all_files():
+  files = []
+  for device_folder in devices_folders:
+    files.append(device_folder + '/w1_slave')
+  return files
+
+devices_files = list_all_files()
+
+def read_temp_raw(file):
+  f = open(file, 'r')
   lines = f.readlines()
   f.close()
   return lines
 
 def read_temp():
-  lines = read_temp_raw()
-  while lines[0].strip()[-3:] != 'YES':
-    time.sleep(0.2)
-    lines = read_temp_raw()
+  index = 0
+  timestamp = datetime.now().strftime("%y-%m-%d-%H:%M:%S")
+  message = {timestamp: []}
+
+  for file in devices_files:
+    lines = read_temp_raw(file)
+    index += 1
+    
+    while lines[0].strip()[-3:] != 'YES':
+      time.sleep(0.2)
+      lines = read_temp_raw(file)
   
-  equals_pos = lines[1].find('t=')
+    equals_pos = lines[1].find('t=')
   
-  if equals_pos != -1:
-    timestamp = int(time.time())
-    temp_string = lines[1][equals_pos+2:]
-    temp_c = float(temp_string) / 1000.0
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    message = {'temp_c': temp_c, 'temp_f': temp_f, 'time': timestamp}
-    return message
+    if equals_pos != -1:
+      temp_string = lines[1][equals_pos+2:]
+      
+      temp_c = float(temp_string) / 1000.0
+      temp_f = temp_c * 9.0 / 5.0 + 32.0
+      now = int(time.time())
+      data = {'device': index, 'temp_c': temp_c, 'temp_f': temp_f, 'time': now}
+      message[timestamp].append(data)
+  return message
 
 #messages = []
 #batch = 0
