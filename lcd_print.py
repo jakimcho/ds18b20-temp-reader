afe9c37b23168963d37d9c07717b0ff7132ae96d
yakim-rachev <jakimcho@gmail.com>
2016-01-04 11:04:20 +0000

    Finished LCD temperature displaying feature

--- lcd_print.py
+++ lcd_print.py
@@ -6,27 +6,31 @@ import transmit_msg
 from time import sleep
 
 lcd = lcd_display.Adafruit_CharLCD()
-lcd.clear()
-
-msgs = [];
-messages = ds18b20.read_temp()
-
-for m in messages:
-    sensor_id = m.get('sensor_id')
-    room_name = transmit_msg.get_room_by_sensor(sensor_id)
-    data      = m.get('data')[0]
-    time      = str(data['time'])
-    temp_c    = "%.2fC" % data['temp_c']
-
-
-    msgs.append("%s\n%s - %s" % (time, room_name, temp_c))
-    lcd.message(msgs[0])
-    print(msgs)
-
+short_sleep = 5
+long_sleep = 3600
+counter = 0
 
 try:
-    while 1:
-        sleep(1)
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+  while 1:
+    messages = ds18b20.read_temp()
+    
+    for m in messages:
+      lcd.clear()
+      sensor_id = m.get('sensor_id')
+      room_name = transmit_msg.get_room_by_sensor(sensor_id)
+      data = m.get('data')[0]
+      time = str(data['time'])
+      temp_c = "%.2fC" % data['temp_c']
+      
+      message = "%s\n%s: %s" % (time, room_name, temp_c)
+      lcd.message(message)
+      sleep(short_sleep)
+      
+  counter += 1
+  if counter == 2:
+    counter = 0
+    sleep(long_sleep)   
+     
+except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
     lcd.clear()
     lcd.clean_gpio()
--- transmit_msg.py
+++ transmit_msg.py
@@ -2,7 +2,11 @@ import pymongo
 import json
 
 from pymongo import MongoClient
-rooms = {'room1': '28-0000072f378f', 'room2': '28-000006c9fdf1', "room3": "28-0000"}
+rooms = {'room1': '28-0000072f378f', 
+         'room2': '28-000006c9fdf1', 
+         "room3": "28-0000072e65a3", 
+         "room4": "28-000006cade26", 
+         "room5": "28-0000072f0f6b"}
 MONGO_URL = 'mongodb://jakim:12345@ds059654.mongolab.com:59654/mongojr'
 
 def send(messages):
@@ -18,7 +22,6 @@ def send(messages):
     
     if not room_exists(room_name, rooms_collection):
       record_room_id = add_new_room(room_name, sensor_id, rooms_collection)
-      print(record_room_id)
     
     record_id = append_temperature_to_room(room_name, data, rooms_collection)
     
@@ -26,8 +29,6 @@ def send(messages):
   return record_id
 
 def get_room_by_sensor(sensor_id):
-  print (sensor_id)
-  print (rooms)
   for room, sensor in rooms.items():
     if sensor == sensor_id:
       return room
